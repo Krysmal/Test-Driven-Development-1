be06db42671a77870eff7a2469faa929c78bb699
Krzysztof Malinowski <malinowskizxcvbnm@gmail.com>
2025-04-19 17:22:24 +0200

    design done

--- Test-Driven-Development-1/Form1.Designer.cs
+++ Test-Driven-Development-1/Form1.Designer.cs
@@ -1,16 +1,18 @@
 ﻿namespace Test_Driven_Development_1
+{ 
+partial class Form1
 {
-    partial class Form1
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
 
         /// <summary>
         ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -19,7 +21,7 @@
             }
             base.Dispose(disposing);
         }
-
+        
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,12 +30,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            buttonC = new Button();
+            buttonEQ = new Button();
+            buttonADD = new Button();
+            buttonSUB = new Button();
+            buttonDIV = new Button();
+            buttonMUL = new Button();
+            panel1 = new Panel();
+            display = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 90);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(44, 38);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(88, 90);
+            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(44, 38);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(137, 90);
+            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(44, 38);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(39, 132);
+            button4.Margin = new Padding(3, 2, 3, 2);
+            button4.Name = "button4";
+            button4.Size = new Size(44, 38);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(88, 132);
+            button5.Margin = new Padding(3, 2, 3, 2);
+            button5.Name = "button5";
+            button5.Size = new Size(44, 38);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(137, 132);
+            button6.Margin = new Padding(3, 2, 3, 2);
+            button6.Name = "button6";
+            button6.Size = new Size(44, 38);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(39, 174);
+            button7.Margin = new Padding(3, 2, 3, 2);
+            button7.Name = "button7";
+            button7.Size = new Size(44, 38);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(88, 174);
+            button8.Margin = new Padding(3, 2, 3, 2);
+            button8.Name = "button8";
+            button8.Size = new Size(44, 38);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(137, 174);
+            button9.Margin = new Padding(3, 2, 3, 2);
+            button9.Name = "button9";
+            button9.Size = new Size(44, 38);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            button0.Location = new Point(88, 216);
+            button0.Margin = new Padding(3, 2, 3, 2);
+            button0.Name = "button0";
+            button0.Size = new Size(44, 38);
+            button0.TabIndex = 10;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            // 
+            // buttonC
+            // 
+            buttonC.Location = new Point(39, 216);
+            buttonC.Margin = new Padding(3, 2, 3, 2);
+            buttonC.Name = "buttonC";
+            buttonC.Size = new Size(44, 38);
+            buttonC.TabIndex = 11;
+            buttonC.Text = "C";
+            buttonC.UseVisualStyleBackColor = true;
+            // 
+            // buttonEQ
+            // 
+            buttonEQ.Location = new Point(137, 216);
+            buttonEQ.Margin = new Padding(3, 2, 3, 2);
+            buttonEQ.Name = "buttonEQ";
+            buttonEQ.Size = new Size(44, 38);
+            buttonEQ.TabIndex = 12;
+            buttonEQ.Text = "=";
+            buttonEQ.UseVisualStyleBackColor = true;
+            // 
+            // buttonADD
+            // 
+            buttonADD.Location = new Point(186, 90);
+            buttonADD.Margin = new Padding(3, 2, 3, 2);
+            buttonADD.Name = "buttonADD";
+            buttonADD.Size = new Size(44, 38);
+            buttonADD.TabIndex = 13;
+            buttonADD.Text = "+";
+            buttonADD.UseVisualStyleBackColor = true;
+            // 
+            // buttonSUB
+            // 
+            buttonSUB.Location = new Point(186, 132);
+            buttonSUB.Margin = new Padding(3, 2, 3, 2);
+            buttonSUB.Name = "buttonSUB";
+            buttonSUB.Size = new Size(44, 38);
+            buttonSUB.TabIndex = 14;
+            buttonSUB.Text = "-";
+            buttonSUB.UseVisualStyleBackColor = true;
+            // 
+            // buttonDIV
+            // 
+            buttonDIV.Location = new Point(186, 216);
+            buttonDIV.Margin = new Padding(3, 2, 3, 2);
+            buttonDIV.Name = "buttonDIV";
+            buttonDIV.Size = new Size(44, 38);
+            buttonDIV.TabIndex = 16;
+            buttonDIV.Text = "/";
+            buttonDIV.UseVisualStyleBackColor = true;
+            // 
+            // buttonMUL
+            // 
+            buttonMUL.Location = new Point(186, 174);
+            buttonMUL.Margin = new Padding(3, 2, 3, 2);
+            buttonMUL.Name = "buttonMUL";
+            buttonMUL.Size = new Size(44, 38);
+            buttonMUL.TabIndex = 15;
+            buttonMUL.Text = "*";
+            buttonMUL.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(display);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(buttonDIV);
+            panel1.Controls.Add(buttonMUL);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(buttonSUB);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(buttonADD);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(buttonEQ);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(buttonC);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button0);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button8);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(285, 338);
+            panel1.TabIndex = 17;
+            // 
+            // display
+            // 
+            display.Enabled = false;
+            display.Location = new Point(39, 35);
+            display.Name = "display";
+            display.Size = new Size(191, 23);
+            display.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(285, 338);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+
         }
 
         #endregion
+        private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button0;
+    private Button buttonC;
+    private Button buttonEQ;
+    private Button buttonADD;
+    private Button buttonSUB;
+    private Button buttonDIV;
+    private Button buttonMUL;
+    private Panel panel1;
+        private TextBox display;
     }
 }
