adc608d35dc50bb2b303e13c5e9af783571a6507
Krzysztof Malinowski <malinowskizxcvbnm@gmail.com>
2025-04-22 13:09:49 +0200

    final

--- Test-Driven-Development-1/Form1.cs
+++ Test-Driven-Development-1/Form1.cs
@@ -10,10 +10,10 @@ namespace Test_Driven_Development_1
 
         public bool part = false;
         public string text = string.Empty;
-        public float num1 = 0;
-        public float num2 = 0;
+        public double num1 = 0;
+        public double num2 = 0;
         public int type = 0;
-        public float result = 0;
+        public double result = 0;
 
 
         public void write(string text)
@@ -176,48 +176,162 @@ namespace Test_Driven_Development_1
         public void buttonC_Click(object sender, EventArgs e)
         {
             display.Text = string.Empty;
+            text=string.Empty;
+            part= false;
             num1 = 0;
             num2 = 0;
+            write(text);
         }
 
         public void buttonADD_Click(object sender, EventArgs e)
         {
+            if (!part)
+            {
+                type = 1;
+                text += " + ";
+                part = true;
+                write(text);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("podaj drug¹ liczbê");
+            }
+            
 
         }
 
         public void buttonSUB_Click(object sender, EventArgs e)
         {
-
+            if (!part)
+            {
+                type = 2;
+                text += " - ";
+                part = true;
+                write(text);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("podaj drug¹ liczbê");
+            }
         }
 
         public void buttonMUL_Click(object sender, EventArgs e)
         {
-
+            if (!part)
+            {
+                type = 3;
+                text += " * ";
+                part = true;
+                write(text);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("podaj drug¹ liczbê");
+            }
         }
 
         public void buttonDIV_Click(object sender, EventArgs e)
         {
-
+            if (!part)
+            {
+                type = 4;
+                text += " / ";
+                part = true;
+                write(text);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("podaj drug¹ liczbê");
+            }
         }
 
         public void buttonEQ_Click(object sender, EventArgs e)
         {
-
+            switch (type)
+            {
+                case 1:
+                    result = num1 + num2;
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    break;
+                case 4:
+                    if(num2!=0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        System.Windows.Forms.MessageBox.Show("nie mo¿na dzieliæ przez 0");
+                        
+                    }
+                        break;
+                case 5:
+                    result = Math.Pow(num1, num2);
+                    break;
+                case 6:
+                    result = Math.Pow(num1, 1.0 / num2);
+                    break;
+                case 7:
+                    result = Math.Log(num1) / Math.Log(num2);
+                    break;
+                default:
+                    throw new ArgumentException("Nieznany typ operacji.");
+            }
+            num1 = 0;
+            num2 = 0;
+            text=string.Empty;
+            type = 0;
+            part=false;
+            write(result.ToString());
         }
 
         public void buttonPOW_Click(object sender, EventArgs e)
         {
-
+            if (!part)
+            {
+                type = 5;
+                text += " ^ ";
+                part = true;
+                write(text);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("podaj drug¹ liczbê");
+            }
         }
 
         public void buttonROOT_Click(object sender, EventArgs e)
         {
-
+            if (!part)
+            {
+                type = 6;
+                text += " ^1/ ";
+                part = true;
+                write(text);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("podaj drug¹ liczbê");
+            }
         }
 
         public void buttonLOG_Click(object sender, EventArgs e)
         {
-
+            if (!part)
+            {
+                type = 7;
+                text += " LOG ";
+                part = true;
+                write(text);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("podaj drug¹ liczbê");
+            }
         }
     }
 }
